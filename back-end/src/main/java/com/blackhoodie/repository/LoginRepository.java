2e5e99924b7c58a04eea6af54d34301d73b67f9b
black-hoodie <tungvoson98@gmail.com>
2023-02-12 15:17:23 +0700

    add server

---
+++ back-end/src/main/java/com/blackhoodie/repository/LoginRepository.java
@@ -0,0 +1,9 @@
+package com.blackhoodie.repository;
+
+import com.blackhoodie.model.Login;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LoginRepository extends JpaRepository<Login, Long> {
+}

7c81875d35a82512b19873de8b35cf01edbe7dbe
black-hoodie <tungvoson98@gmail.com>
2023-02-12 23:52:01 +0700

    update sign up api

--- back-end/src/main/java/com/blackhoodie/controller/login/LoginController.java
+++ back-end/src/main/java/com/blackhoodie/controller/login/LoginController.java
@@ -1,17 +1,42 @@
 package com.blackhoodie.controller.login;
 
 import com.blackhoodie.model.Login;
-import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import com.blackhoodie.model.User;
+import com.blackhoodie.repository.UserRepository;
+import com.blackhoodie.request.SignUpRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1")
 public class LoginController implements LoginInterface{
 
+    @Autowired
+    UserRepository userRepository;
+
     @PostMapping("/login")
     public Boolean login(Login loginInfo) {
         return false;
+    }
+
+    @PostMapping("/signup")
+    public Boolean signup(SignUpRequest req) {
+        try {
+            User user = new User(
+                    req.getName(),
+                    req.getGender(),
+                    req.getPhoneNumber(),
+                    req.getEmail()
+            );
+            userRepository.save(user);
+            return true;
+        } catch (Exception e) {
+            System.out.println(e);
+            return false;
+        }
     }
 }
--- back-end/src/main/java/com/blackhoodie/controller/login/LoginInterface.java
+++ back-end/src/main/java/com/blackhoodie/controller/login/LoginInterface.java
@@ -1,10 +1,13 @@
 package com.blackhoodie.controller.login;
 
 import com.blackhoodie.model.Login;
+import com.blackhoodie.model.User;
+import com.blackhoodie.request.SignUpRequest;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 public interface LoginInterface {
 
     public Boolean login(@RequestBody Login loginInfo);
+
+    public Boolean signup(@RequestBody SignUpRequest req);
 }

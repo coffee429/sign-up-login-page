ec423d86483e1f21e69831093f6ea78ffbc1771b
black-hoodie <tungvoson98@gmail.com>
2023-02-15 21:55:43 +0700

    refactor code

--- back-end/src/main/java/com/blackhoodie/model/Account.java
+++ back-end/src/main/java/com/blackhoodie/model/Account.java
@@ -3,10 +3,11 @@ package com.blackhoodie.model;
 import jakarta.persistence.*;
 
 @Entity
+@Table(name = "account")
 public class Account {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "email", nullable = false)
@@ -14,4 +15,12 @@ public class Account {
 
     @Column(name = "password", nullable = false)
     private String password;
+
+    public Account() {
+    }
+
+    public Account(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
 }
